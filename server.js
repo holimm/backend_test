6b33f18f3e78bf695f442a69dc07c7777a68e4d1
holimm <kahn12345678@gmail.com>
2023-07-05 11:50:08 +0700

    Test

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.post("/api/rpa-uipath", async (req, res) => {
       .collection("UIPath")
       .insertOne(req.body, (err) => {
         if (err) throw err;
-        // res.send({ status: "success", message: "Gửi yêu cầu thành công!" });
+        res.send({ status: "success", message: "Gửi yêu cầu thành công!" });
         console.log("Data has been pushed successfully!");
       });
     // console.log(req.body.autoReponse);
@@ -76,7 +76,7 @@ app.post("/api/rpa-uipath", async (req, res) => {
   } catch (err) {
     console.log(err);
     // res.send({ status: "success", message: "Failure" });
-    // res.send({ status: "fail", message: "Gửi yêu cầu thất bại!" });
+    res.send({ status: "fail", message: "Gửi yêu cầu thất bại!" });
   }
 });
 
@@ -93,6 +93,7 @@ app.get("/api/rpa-uipath/get", async (req, res) => {
         console.log("Get UIPath Email List Successfully!");
       });
   } catch (err) {
+    res.send(err);
     console.log(err);
   }
 });
