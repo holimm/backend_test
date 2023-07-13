eda0812a857576d670bad898fd9ee827e37c0b5f
holimm <kahn12345678@gmail.com>
2023-07-13 15:51:29 +0700

    test

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ var express = require("express");
 var path = require("path");
 var multer = require("multer");
 // var db = require('./database')
-app.use(
-  multer({
-    dest: "uploads/",
-  })
-);
+
 const bcrypt = require("bcrypt");
 const PORT = 8080;
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -17,6 +13,7 @@ var app = express();
 app.listen(process.env.PORT || PORT, function () {
   console.log(`started listen port ${PORT}`);
 });
+
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 var cors = require("cors");
@@ -103,10 +100,37 @@ app.get("/api/rpa-uipath/get", async (req, res) => {
   }
 });
 
+const maxSize = 10 * 1000 * 1000;
+
+var upload = multer({
+  storage: null,
+  limits: { fileSize: maxSize },
+  fileFilter: function (req, file, cb) {
+    var filetypes = /jpeg|jpg|png/;
+    var mimetype = filetypes.test(file.mimetype);
+    var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+    cb(
+      "Error: File upload only supports the " +
+        "following filetypes - " +
+        filetypes
+    );
+  },
+}).single("image");
+
 app.post("/api/file-upload/post", async (req, res) => {
   try {
-    console.log(req.files);
-    res.send({ status: "success", content: "Thành công" });
+    upload(req, res, function (err) {
+      if (err) {
+        console.log(err);
+        res.send({ status: "error", content: "Lỗi khi đăng hình ảnh" });
+      } else {
+        console.log(req.file);
+        res.send({ status: "valid", content: req.file.filename });
+      }
+    });
   } catch (err) {
     res.send({ status: "error", message: "Gửi yêu cầu thất bại!" });
     console.log(err);
